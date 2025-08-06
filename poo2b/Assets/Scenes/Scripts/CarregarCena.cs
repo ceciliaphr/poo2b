f4e35d79ee91cb3b7e6931509c869d9432e9740f
ceciliaphr <cecilia.pinheiro@escolar.ifrn.edu.br>
2025-08-06 16:27:16 -0300

    painel

---
+++ poo2b/Assets/Scenes/Scripts/CarregarCena.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarregarCena : MonoBehaviour
+{
+
+    public string nomeDaCena;
+
+    public void Carregar()
+    {
+        SceneManager.LoadScene(nomeDaCena);
+    }
+
+}    
